ac6db26fa6d912c16dc0adafb733c9868d997fb9
koba-masa <kobayashi_mshr@koba-masa.com>
2023-12-30 09:10:07 +0900

    refactor: バケット直下にプレフィックスを追加する

--- tests/models/aws/test_s3_client.py
+++ tests/models/aws/test_s3_client.py
@@ -17,37 +17,37 @@ def test_init() -> None:
     ],
     [
         pytest.param(
-            "",
+            "sample/",
             1,
             [
-                "AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/28/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231228T2200Z_1.2.3.4_DFXV1dHFH.log.gz",
-                "AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/28/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231228T2200Z_1.2.3.5_lofR4gdsgfs.log.gz",
-                "AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/29/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231229T2340Z_1.2.3.4_ddGdfda12.log.gz",
-                "AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/29/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231229T2345Z_1.2.3.4_hfg343GVBS.log.gz",
-            ],
-        ),
-        pytest.param(
-            "",
-            3,
-            [
-                "AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/28/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231228T2200Z_1.2.3.4_DFXV1dHFH.log.gz",
-                "AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/28/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231228T2200Z_1.2.3.5_lofR4gdsgfs.log.gz",
-                "AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/29/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231229T2340Z_1.2.3.4_ddGdfda12.log.gz",
-                "AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/29/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231229T2345Z_1.2.3.4_hfg343GVBS.log.gz",
-            ],
-        ),
-        pytest.param(
-            "",
-            1000,
-            [
-                "AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/28/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231228T2200Z_1.2.3.4_DFXV1dHFH.log.gz",
-                "AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/28/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231228T2200Z_1.2.3.5_lofR4gdsgfs.log.gz",
-                "AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/29/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231229T2340Z_1.2.3.4_ddGdfda12.log.gz",
-                "AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/29/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231229T2345Z_1.2.3.4_hfg343GVBS.log.gz",
+                "sample/AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/28/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231228T2200Z_1.2.3.4_DFXV1dHFH.log.gz",
+                "sample/AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/28/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231228T2200Z_1.2.3.5_lofR4gdsgfs.log.gz",
+                "sample/AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/29/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231229T2340Z_1.2.3.4_ddGdfda12.log.gz",
+                "sample/AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/29/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231229T2345Z_1.2.3.4_hfg343GVBS.log.gz",
             ],
         ),
         pytest.param(
             "sample/",
+            3,
+            [
+                "sample/AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/28/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231228T2200Z_1.2.3.4_DFXV1dHFH.log.gz",
+                "sample/AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/28/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231228T2200Z_1.2.3.5_lofR4gdsgfs.log.gz",
+                "sample/AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/29/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231229T2340Z_1.2.3.4_ddGdfda12.log.gz",
+                "sample/AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/29/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231229T2345Z_1.2.3.4_hfg343GVBS.log.gz",
+            ],
+        ),
+        pytest.param(
+            "sample/",
+            1000,
+            [
+                "sample/AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/28/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231228T2200Z_1.2.3.4_DFXV1dHFH.log.gz",
+                "sample/AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/28/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231228T2200Z_1.2.3.5_lofR4gdsgfs.log.gz",
+                "sample/AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/29/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231229T2340Z_1.2.3.4_ddGdfda12.log.gz",
+                "sample/AWSLogs/123456789012/elasticloadbalancing/ap-northeast-1/2023/12/29/123456789012_elasticloadbalancing_ap-northeast-1_app.sample_20231229T2345Z_1.2.3.4_hfg343GVBS.log.gz",
+            ],
+        ),
+        pytest.param(
+            "example/",
             1,
             [],
         ),
