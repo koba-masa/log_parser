36ee779041c0c6ffdb1c01ea94f5743300902381
koba-masa <kobayashi_mshr@koba-masa.com>
2023-12-30 11:14:21 +0900

    feat: テスト実行時にディレクトリを削除するfixtureを追加する

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,13 @@
+import pytest
+import shutil
 from models import settings
 
 
 settings.load_config("config/test.yaml")
+
+
+@pytest.fixture(scope="function")
+def delete_dir() -> None:
+    yield
+
+    shutil.rmtree("tmp/tests")
