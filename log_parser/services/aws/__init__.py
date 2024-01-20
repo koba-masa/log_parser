3120a8b3648383057245ec72d92f76f52ce2a4af
koba-masa <kobayashi_mshr@koba-masa.com>
2024-01-20 18:55:41 +0900

    feat: AWS用サービスクラスで使用する基底クラスを追加する

--- log_parser/services/aws/__init__.py
+++ log_parser/services/aws/__init__.py
@@ -1,3 +1,5 @@
+from .base import Base
 from .aplication_load_blancer import ApplicationLoadBalancer
+from .cloudwatch_metric import CloudWatchMetric
 
-__all__ = ["ApplicationLoadBalancer"]
+__all__ = ["Base", "ApplicationLoadBalancer"]
