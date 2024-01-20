a058331e263aba12f42229e40d5ae34aa8a6c2c6
koba-masa <kobayashi_mshr@koba-masa.com>
2024-01-20 19:01:19 +0900

    fix: ファイル名を修正する

--- log_parser/services/aws/__init__.py
+++ log_parser/services/aws/__init__.py
@@ -1,5 +1,5 @@
 from .base import Base
-from .aplication_load_blancer import ApplicationLoadBalancer
+from .application_load_blancer import ApplicationLoadBalancer
 from .cloudwatch_metric import CloudWatchMetric
 
 __all__ = ["Base", "ApplicationLoadBalancer", "CloudWatchMetric"]
